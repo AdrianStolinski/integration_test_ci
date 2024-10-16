6f12336eeec27b7634b9d719f8902237f2759dc8
EdwardWes <140832643+EdwardWes@users.noreply.github.com>
2024-10-16 13:51:46 +0200

    first attempt (#2)

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,3 +1,4 @@
+import 'package:counter_app/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
@@ -8,7 +9,15 @@ void main() {
     testWidgets(
       'tap on the floating action button, verify counter',
       (tester) async {
-        throw UnimplementedError();
+        await tester.pumpWidget(const App());
+
+        expect(find.text('0'), findsOneWidget);
+
+        final Finder fab = find.byTooltip('Increment');
+        await tester.tap(fab);
+
+        await tester.pumpAndSettle();
+        expect(find.text('1'), findsOneWidget);
       },
     );
   });
